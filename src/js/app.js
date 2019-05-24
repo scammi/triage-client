d325a67a257333370c35efcbf305ad3d11db7288
scammi <scammi@gmail.com>
2019-05-24 11:33:55 -0300

    test to get data async

--- src/js/app.js
+++ src/js/app.js
@@ -1,28 +1,43 @@
-$(document).ready(function(){
+let datas;
 
+async function getData(){
+	await $.ajax({
+			 url: 'https://randomuser.me/api/',
+			 dataType: 'json',
+			 success: function(data) {
+				 	datas = data;
+
+
+				 //let name = data.results[0].name.first + " " + data.results[0].name.last;
+				 //age = (data.results[0].dob.age).toString();
+			 //	$("#name").html(name);
+				 //$("#patient").html(name);
+					//$("#age").html(age);
+			 }
+		 });
+
+	}
+
+
+
+$(document).ready(function(){
 	/*
 		Gets ramdom user json data
 		Saves name and age into vars
 		Finds by id and displys in html
 	*/
-	$.ajax({
-		url: 'https://randomuser.me/api/',
-		dataType: 'json',
-		success: function(data) {
-			let name = data.results[0].name.first + " " + data.results[0].name.last;
-			let age = (data.results[0].dob.age).toString();
-			$("#name").html(name);
-			$("#patient").html(name);
-			$("#age").html(age);
-			console.log(age);
 
-		}
-	});
+async function getAge(){
+await getData()
+console.log(datas)
+}
+
+getAge();
 
 //Filter patients by triage
 	$("button").click(function(){
 		var inputValue = $(this).val();
-		console.log(inputValue)
+		console.log(age);
 		if(inputValue != 'All'){
 			var target = $('table tr[data-status="' + inputValue + '"]');
 			$("table tbody tr").not(target).hide();
