70c5185dc2e68953d9a18b40166d81879233a2d2
scammi <scammi@gmail.com>
2019-05-23 21:55:34 -0300

    added mobile compa

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,16 @@
-let name;
 $(document).ready(function(){
 
+	/*
+		Gets ramdom user json data
+		Saves name and age into vars
+		Finds by id and displys in html
+	*/
 	$.ajax({
 		url: 'https://randomuser.me/api/',
 		dataType: 'json',
 		success: function(data) {
-			name = data.results[0].name.first + " " + data.results[0].name.last;
-			age = (data.results[0].dob.age).toString();
+			let name = data.results[0].name.first + " " + data.results[0].name.last;
+			let age = (data.results[0].dob.age).toString();
 			$("#name").html(name);
 			$("#patient").html(name);
 			$("#age").html(age);
@@ -16,9 +20,10 @@ $(document).ready(function(){
 	});
 
 //Filter patients by triage
-	$(".btn-group .btn").click(function(){
-		var inputValue = $(this).find("input").val();
-		if(inputValue != 'all'){
+	$("button").click(function(){
+		var inputValue = $(this).val();
+		console.log(inputValue)
+		if(inputValue != 'All'){
 			var target = $('table tr[data-status="' + inputValue + '"]');
 			$("table tbody tr").not(target).hide();
 			target.fadeIn();
